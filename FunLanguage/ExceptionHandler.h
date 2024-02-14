ae9b1b0f61fd1044e3850f4b023a265e1fcd2c2f
qasmark <99826972+qasmark@users.noreply.github.com>
2024-02-14 19:11:37 +0300

    init commit
    added startup files for project

---
+++ FunLanguage/ExceptionHandler.h
@@ -0,0 +1,4 @@
+#pragma once
+#include <exception>
+
+// H-file for handle expection in modules
---
+++ FunLanguage/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <fstream>
+
+int main()
+{
+	std::ifstream fin("simple-example-of-code.txt");
+
+	return 0;
+}
+// Visual Studio Community 2022
