a95b1346cf060c7ea92d9d49fd94f9e4ea1b9498
den990 <200321ds>
2024-02-17 14:58:41 +0300

    Add branch

---
+++ FunLanguage/ReadAndWritAndListStatements.h
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+bool Read(std::istream& file, std::string& str)
+{
+	return getline(file, str);
+	
+}
+
+void Write()
+{
+
+}
+
+void ListStatements()
+{
+	// return State || ListStatements()
+}
