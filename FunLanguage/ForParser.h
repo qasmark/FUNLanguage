9160f834b9ec5d0b68f15d284ae12b9cf9bba39d
amfunNyLORD <funnybulletgames@yandex.ru>
2024-02-25 15:42:52 +0300

    Изменил парсинг for, вынес в константы, добавил stringstream

--- FunLanguage/ForParser.h
+++ FunLanguage/ForParser.h
@@ -1,68 +1,66 @@
 #pragma once
 #include <iostream>
+#include <sstream>
 
-bool ParseFOR(std::string& line)
+const std::string FOR_TERMINAL = "FOR";
+const std::string ASSIGNMENT_TERMINAL = ":=";
+const std::string TO_TERMINAL = "TO";
+const std::string DO_TERMINAL = "DO";
+
+void DeleteSpaces(std::string& line)
 {
 	line.erase(remove(line.begin(), line.end(), ' '), line.end());
-	if (line.substr(0, 4) != "for(")
-	{
-		return false;
-	}
-	line.erase(0, 4);
-	if (!ParseIdent(line))
-	{
-		return false;
-	}
-	if (line[0] != '=')
-	{
-		return false;
-	}
-	line.erase(line.begin());
-	if (!ParseValue)
-	{
-		return false;
-	}
-	if (line[0] != ';')
-	{
-		return false;
-	}
-	line.erase(line.begin());
-	if (!ParseIdent(line) || !ParseRelation(line) || !ParseValue(line))
-	{
-		return false;
-	}
-	if (line[0] != ';')
-	{
-		return false;
-	}
-	if (!ParseIdent(line))
-	{
-		return false;
-	}
-	if (line[0] != '=')
-	{
-		return false;
-	}
-	if (!ParseIdent(line) || !ParseMul(line) || !ParseValue(line))
-	{
-		return false;
-	}
-	if (line[0] != ')')
-	{
-		return false;
-	}
-	line.erase(line.begin());
-	if (!ParseSt(line))
-	{
-		return false;
-	}
-	return true;
 }
 
-bool ParseValue(std::string& line)
+bool ParseFOR(std::stringstream& strm)
 {
-	if (!ParseNumb(line) || !ParseIdent(line))
+	std::string line;
+	strm >> line;
+
+	DeleteSpaces(line);
+
+	if (line.substr(0, 3) != FOR_TERMINAL)
 	{
+		std::cout << "Syntax error: expected \'" + FOR_TERMINAL + "\', line: pos:" << std::endl; // from lexer
+		return false;
+	}
+	line.erase(0, 3);
+	if (!ParseIdent(line))
+	{
+		std::cout << "Syntax error: expected \'identifier\', line: pos:" << std::endl; // from lexer
+		return false;
+	}
+	if (line.substr(0, 2) != ASSIGNMENT_TERMINAL)
+	{
+		std::cout << "Syntax error: expected \'" + ASSIGNMENT_TERMINAL + "\', line: pos:" << std::endl; // from lexer
+		return false;
+	}
+	line.erase(0, 2);
+	if (!ParseSimpleExpr)
+	{
+		std::cout << "Syntax error: expected \'simple expression\', line: pos:" << std::endl; // from lexer
+		return false;
+	}
+	if (line.substr(0, 2) != TO_TERMINAL)
+	{
+		std::cout << "Syntax error: expected \'" + TO_TERMINAL + "\', line: pos:" << std::endl; // from lexer
+		return false;
+	}
+	line.erase(0, 2);
+	if (!ParseSimpleExpr)
+	{
+		std::cout << "Syntax error: expected \'simple expression\', line: pos:" << std::endl; // from lexer
+		return false;
+	}
+	if (line.substr(0, 2) != DO_TERMINAL)
+	{
+		std::cout << "Syntax error: expected \'" + DO_TERMINAL + "\', line: pos:" << std::endl; // from lexer
+		return false;
+	}
+	line.erase(0, 2);
+	if (!ParseSt(line))
+	{
+		std::cout << "Syntax error: expected \'statement\', line: pos:" << std::endl; // from lexer
 		return false;
 	}
 	return true;
